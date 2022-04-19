73dc2583ee4113a62420c5fcd44ef11b472338fa
Maiaguiter <49686906@est.ort.edu.ar>
2022-04-19 08:57:25 -0300

    finished

---
+++ Assets/prueba.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prueba : MonoBehaviour
+{
+    public string Gusto;
+    public int Cantidad;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float precio = Cantidad * 500 / 1000 / 2;
+        float preciofrutilla = precio / 10;
+        float preciodescuento = precio + preciofrutilla;
+        string gusto = "CHO";
+        string gusto2 = "FRU";
+        string gusto3 = "DDL";
+
+ 
+        if (gusto != "CHO"){
+            Debug.Log("El codigo no es valido");
+        }
+        if (gusto != "FRU")
+        {
+            Debug.Log("El codigo no es valido");
+        }
+        if (gusto != "DDL")
+        {
+            Debug.Log("El codigo no es valido");
+        }
+        if (Cantidad > 3000 || Cantidad < 250)
+        {
+            Debug.Log("La cantidad no es valida");
+        }
+        else
+        {
+            if (gusto == "CHO")
+            {
+                Debug.Log("Su precio es de " + precio + " pesos");
+            }
+            if (gusto3 == "DDL")
+            {
+                Debug.Log("Su precio es de " + precio + " pesos");
+            }
+            if (gusto2 == "FRU")
+            {
+                Debug.Log("Su precio es de " + preciofrutilla + " pesos");
+            }
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
